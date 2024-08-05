e3038989cb0d2157901604f2b69658375c2994b1
Frank <desenvolvimento@criciuma.sc.gov.br>
2024-08-05 11:19:25 -0300

    ExemploDoWhile

---
+++ fundamentosDoJava/src/je10_controle_fluxo_repeticao/ExemploDoWhile.java
@@ -0,0 +1,22 @@
+package je10_controle_fluxo_repeticao;
+
+import java.util.Random;
+
+public class ExemploDoWhile {
+    public static void main(String[] args) {
+        System.out.println("Discando...");
+
+        do {
+            //executando repetidas vezes até alguem atender
+            System.out.println("Telefone tocando - trim trim");
+        } while(tocando());
+
+        System.out.println("Alô !!!");
+    }
+    private static boolean tocando() {
+        boolean atendeu = new Random().nextInt(3)==1;
+        System.out.println("Atendeu? " + atendeu);
+        //negando o ato de continuar tocando
+        return ! atendeu;
+    }
+}
