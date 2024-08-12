096691382ffcf5780657bdec52dd447ee3ee9012
Frank <desenvolvimento@criciuma.sc.gov.br>
2024-08-12 11:41:18 -0300

    Sample JavaFileIO

---
+++ fundamentosDoJava/Java N-IO/src/je26_java_nio/JavaFileIO.java
@@ -0,0 +1,25 @@
+package je26_java_nio;
+
+import java.io.File;
+import java.io.IOException;
+
+public class JavaFileIO {
+    public static void main(String[] args) {
+        File diretorio = new File("c:\\rocket");
+        System.out.println("diretorio existe?" + diretorio.exists());
+        if(!diretorio.exists()) {
+            diretorio.mkdirs();
+            System.out.println("foi criado o diretorio");
+        }
+
+        try {
+            File arquivo = new File(diretorio, "aula-java.txt");
+            System.out.println(arquivo.exists());
+
+            arquivo.createNewFile();
+            System.out.println(arquivo.exists());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
