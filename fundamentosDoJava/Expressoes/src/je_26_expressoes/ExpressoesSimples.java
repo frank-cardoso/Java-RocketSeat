12a4293cca39e004f03ef5a748b9b8a60af1fb25
Frank <desenvolvimento@criciuma.sc.gov.br>
2024-08-13 13:00:16 -0300

    Sample Expressions

--- fundamentosDoJava/Expressoes/src/je_26_expressoes/ExpressoesSimples.java
+++ fundamentosDoJava/Expressoes/src/je_26_expressoes/ExpressoesSimples.java
@@ -4,7 +4,7 @@ public class ExpressoesSimples {
     public static void main(String[] args) {
         String nome = "Frank";
         Integer matricula = 123;
-        Double salario = 2245.83;
+        Double salario = 2244.83;
 
         String dadosFuncionario = String.format("Mat.: %d - Nome : %s - Sal.: %,.2f", matricula, nome, salario);
         System.out.println(dadosFuncionario);
